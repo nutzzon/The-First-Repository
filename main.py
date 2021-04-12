2db6510e8963da340d0ca6c08358971f14b6ab6c
RCKR <richard.raman@saunalahti.fi>
2021-04-12 17:34:42 +0300

    added comment to main

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ def printmsg(msg):
 
 # this is changed from 435 to hahaha...
 printmsg('hahaha, I have changed this file so you can"t change it back!')
+
+# I'm going to add this comment here to see if I get a merge conflict 
+# the functions file I added
