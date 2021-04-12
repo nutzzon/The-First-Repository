d34c55621b6cf176ee8811bbd4e0ef916157a65e
RCKR <richard.raman@saunalahti.fi>
2021-04-12 17:12:04 +0300

    simulating merge conflict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def printmsg(msg):
     print(str(msg))
 
-
+# this is another thing I made to this file. This can be something else. 
+# the idea here is that it should create a merge conflict
 printmsg(435)
